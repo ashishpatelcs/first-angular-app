346f69a285bedf16b677f7eb53d217adcf351281
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-08-23 09:38:16 +0530

    Add servers to array and display using ngFor directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server created!";
   serverName = 'Testserver';
+  servers = ['Testserver', 'Testserver2'];
   serverCreated = false;
 
   constructor() {
@@ -23,8 +24,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created! Name is " + this.serverName ;
     this.serverCreated = true;
+    this.servers.push(this.serverName) ;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   updateServerName(event: Event) {
